1ceb9869255623b0e1a023620a0277f19cc31172
Piyush <Piyushyadav5282@gmail.com>
2023-02-18 13:31:51 +0530

    first commit

---
+++ prog.java
@@ -0,0 +1,15 @@
+class Browser 
+   { 
+          String urls[];
+          public void setName()
+      {
+      system.out.println(urls);
+      }
+
+     {
+            
+   public static void main(String args[])
+        {
+          Browser obj=new Browser();
+        }
+    }
